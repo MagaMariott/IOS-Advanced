dcf39e853602cd2b108229b04944e8928de55505
MagaMariott <101788722+MagaMariott@users.noreply.github.com>
2025-02-07 04:27:03 +0500

    Add files via upload

---
+++ About Temi/About Temi/About_TemiApp.swift
@@ -0,0 +1,18 @@
+//
+//  About_TemiApp.swift
+//  About Temi
+//
+//  Created by Temirlan Satybaldy on 06.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct About_TemiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
